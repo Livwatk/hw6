a234c339b3f70f3f725277ec4a6fab1bcc73dd19
Olivia W <wolivia2821@gmail.com>
2022-04-17 10:48:31 -0700

    forecast js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,32 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+<div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/02n@2x.png"
+            alt=""
+            width="42"
+            />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max"> 18° </span>
+              <span class="weather-forecast-temperature-min">| 12° </span>
+           </div>
+        </div>
+`;
+  });
+  forecast = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
@@ -78,3 +104,5 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+displayForecast();
